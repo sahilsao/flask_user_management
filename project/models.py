262914338751345a5384307eb3606d41401c4bab
Patrick Kennedy <patkennedy79@gmail.com>
2018-02-05 09:13:07 -0800

    Updated User model

--- project/models.py
+++ project/models.py
@@ -3,28 +3,37 @@ from datetime import datetime
 
 
 class User(db.Model):
+    """
+    Class that represents a user of the application
+
+    The following attributes of a user are stored in this table:
+        email address
+        password (hashed using Bcrypt)
+        authenticated flag (indicates if a user is logged in or not)
+        date that the user registered on
+    """
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.Binary(60), nullable=False)
+    hashed_password = db.Column(db.Binary(60), nullable=False)
     authenticated = db.Column(db.Boolean, default=False)
     registered_on = db.Column(db.DateTime, nullable=True)
     role = db.Column(db.String, default='user')
 
     def __init__(self, email, plaintext_password, role='user'):
         self.email = email
-        self.password = bcrypt.generate_password_hash(plaintext_password)
+        self.hashed_password = bcrypt.generate_password_hash(plaintext_password)
         self.authenticated = False
         self.registered_on = datetime.now()
         self.role = role
 
     def set_password(self, plaintext_password):
-        self.password = bcrypt.generate_password_hash(plaintext_password)
+        self.hashed_password = bcrypt.generate_password_hash(plaintext_password)
 
     def is_correct_password(self, plaintext_password):
-        return bcrypt.check_password_hash(self.password, plaintext_password)
+        return bcrypt.check_password_hash(self.hashed_password, plaintext_password)
 
     @property
     def is_authenticated(self):
