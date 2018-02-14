eba874d55e2ac2850ec13edf9c272735acc8107d
Patrick Kennedy <patkennedy79@gmail.com>
2018-02-13 23:04:53 -0800

    Updated application factory function

--- project/__init__.py
+++ project/__init__.py
@@ -21,11 +21,19 @@ login.login_view = "users.login"
 #### Application Factory Function ####
 ######################################
 
-def create_app(config_filename):
-    # Create the application instance (app) of the Flask framework
+def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_filename)
+    initialize_extensions(app)
+    register_blueprints(app)
+    return app
 
+
+##########################
+#### Helper Functions ####
+##########################
+
+def initialize_extensions(app):
     # Since the application instance is now created, pass it to each Flask
     # extension instance to bind it to the Flask application instance (app)
     db.init_app(app)
@@ -39,10 +47,10 @@ def create_app(config_filename):
     def load_user(user_id):
         return User.query.filter(User.id == int(user_id)).first()
 
-    # Blueprint(s)
+
+def register_blueprints(app):
+    # Since the application instance is now created, register each Blueprint
+    # with the Flask application instance (app)
     from project.users.routes import users_blueprint
 
-    # Register the Blueprint(s)
     app.register_blueprint(users_blueprint)
-
-    return app
