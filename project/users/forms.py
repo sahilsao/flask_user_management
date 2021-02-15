d6c63e002e1b8bd804e8d46a478df36fe9086cbc
Patrick Kennedy <pkennedy@hey.com>
2021-02-15 10:19:19 -0800

    Fixed unused imports in forms.py

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
-from project.models import User
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class RegisterForm(FlaskForm):
