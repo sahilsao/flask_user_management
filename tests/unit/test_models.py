199373de69c94d4038914074ecd0ef96be77b315
Patrick Kennedy <pkennedy@hey.com>
2021-11-22 17:26:27 -0800

    Fixed unit tests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -8,12 +8,11 @@ def test_new_user():
     """
     GIVEN a User model
     WHEN a new User is created
-    THEN check the email, password_hashed, authenticated, and role fields are defined correctly
+    THEN check the email, password_hashed, authenticated, and active fields are defined correctly
     """
     user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
     assert user.email == 'patkennedy79@gmail.com'
     assert user.password_hashed != 'FlaskIsAwesome'
-    assert user.role == 'user'
     assert user.__repr__() == '<User: patkennedy79@gmail.com>'
     assert user.is_authenticated
     assert user.is_active
@@ -24,11 +23,10 @@ def test_new_user_with_fixture(new_user):
     """
     GIVEN a User model
     WHEN a new User is created
-    THEN check the email, password_hashed, authenticated, and role fields are defined correctly
+    THEN check the email and password_hashed fields are defined correctly
     """
     assert new_user.email == 'patkennedy79@gmail.com'
     assert new_user.password_hashed != 'FlaskIsAwesome'
-    assert new_user.role == 'user'
 
 
 def test_setting_password(new_user):
