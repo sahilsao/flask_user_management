7ed14cb5f16c456eb7593343555c96d8494c70b8
Patrick Kennedy <pkennedy@hey.com>
2022-10-03 20:14:49 -0700

    Simplify app initialization

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,7 @@
-import logging
 import os
-from logging.handlers import RotatingFileHandler
 
-import sqlalchemy as sa
 from click import echo
 from flask import Flask
-from flask.logging import default_handler
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -36,25 +32,7 @@ def create_app(config_filename=None):
 
     initialize_extensions(app)
     register_blueprints(app)
-    configure_logging(app)
     register_cli_commands(app)
-
-    app.logger.info(f"DEBUG001... DATABASE_URL environment variable: {os.getenv('DATABASE_URL')}")
-    app.logger.info(f"DEBUG002... CONFIG_TYPE environment variable: {os.getenv('CONFIG_TYPE')}")
-    app.logger.info(f"DEBUG003... SQLALCHEMY_DATABASE_URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-    app.logger.info(f"DEBUG004... LOG_TO_STDOUT environment variable: {os.getenv('LOG_TO_STDOUT')}")
-
-    # Check if the database needs to be initialized
-    engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sa.inspect(engine)
-    if not inspector.has_table("users"):
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
-            app.logger.info('Initialized the database!')
-    else:
-        app.logger.info('Database already contains the users table.')
-
     return app
 
 
@@ -84,27 +62,6 @@ def register_blueprints(app):
 
     app.register_blueprint(recipes_blueprint)
     app.register_blueprint(users_blueprint)
-
-
-def configure_logging(app):
-    # Logging Configuration
-    if app.config['LOG_TO_STDOUT']:
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.INFO)
-        app.logger.addHandler(stream_handler)
-    else:
-        file_handler = RotatingFileHandler('instance/flask-user-management.log',
-                                           maxBytes=16384,
-                                           backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
-
-    app.logger.info('Starting the Flask User Management App...')
 
 
 def register_cli_commands(app):
